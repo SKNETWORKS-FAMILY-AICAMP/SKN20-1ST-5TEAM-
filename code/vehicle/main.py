a0c267a03ef5a75d60f2b5a29aaaa53d89facb1f
Chan Park <qkrcks8028@gmail.com>
2025-09-25 10:12:50 +0900

    refactoring: import 경로 코드

--- code/vehicle/main.py
+++ code/vehicle/main.py
@@ -1,3 +1,7 @@
+import sys 
+import os 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))) 
+
 from vehicle_crawler import VehicleCrawler
 from vehicle_repository import VehicleRepository
 
