41af6bf0ee67bb7be29c86799ced4c8879d6aec2
Raewon12 <fodnjs15@naver.com>
2025-09-25 11:36:18 +0900

    newfile

--- code/main.py
+++ code/main.py
@@ -3,11 +3,11 @@ from faq.main import main as faq_main
 from csv_processor.main import main as csv_main
 
 def main():
-    # print("Vehicle 크롤링 시작")
-    # vehicle_main()
-    # print("FAQ 크롤링 시작")
-    # faq_main()
-    # print("모든 크롤링 완료")
+    print("Vehicle 크롤링 시작")
+    vehicle_main()
+    print("FAQ 크롤링 시작")
+    faq_main()
+    print("모든 크롤링 완료")
     csv_main()
     print("모든 작업 완료")
 
