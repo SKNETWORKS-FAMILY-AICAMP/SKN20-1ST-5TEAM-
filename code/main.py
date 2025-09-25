dd156f853a264ee9e5f703f229070f82ec43a30c
Chan Park <qkrcks8028@gmail.com>
2025-09-25 10:34:38 +0900

    refactorig: crawling 코드 모듈화

--- code/main.py
+++ code/main.py
@@ -1,11 +1,11 @@
-# from code.vehicle.path import main as vehicle_main
-# from faq.main import main as faq_main
+from vehicle.main import main as vehicle_main
+from faq.main import main as faq_main
 
 def main():
     print("Vehicle 크롤링 시작")
-    # vehicle_main()
+    vehicle_main()
     print("FAQ 크롤링 시작")
-    # faq_main()
+    faq_main()
     print("모든 크롤링 완료")
 
 if __name__ == "__main__":
